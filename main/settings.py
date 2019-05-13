cc235547cbb70e084906aca55c92e714721f4ecd
Ehtisham Ashraf <ehtishamashraf91@gmail.com>
2019-05-14 03:23:40 +0500

    Corrected static root and change path of static files

--- main/settings.py
+++ main/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'project-static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 MEDIA_URL = '/media/'
